9c3b1e120b89cb133f1b9eeb0485437e0e83f449
sowenowsky <113459363+sowenowsky@users.noreply.github.com>
2022-09-25 22:07:16 +0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,22 @@
+ABC = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+word = str(input('Введите слово с маленькими буквами: ')) #слово
+wordlist = list (word) #разбивает слово на список
+shift = int(input('Введите желаемый сдвиг: ')) #сдвиг
+k = 0
+result = []
+for i in ABC:
+    if k > len(word) - 1:
+        break
+    else:
+        letter = wordlist[k] #находит нужную букву
+        numberletter = ABC.find(letter)  # буква в алфавите по счёту начиная с 0
+        if numberletter == (-1): #исключает пробелы
+            result.append('')
+        else:
+            result.append((ABC[numberletter + shift])) #добавляет итоговую букву в массив
+        k += 1
+
+converresult = (' '.join(map(str,result))) #преобразование массива в строку
+print ('Итоговое слово: ', converresult)
+
+
